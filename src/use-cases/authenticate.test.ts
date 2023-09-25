511a5790d34e95b093f8c7de0df863e591bc4686
wagnermateus <wagnermateus7@gmail.com>
2023-09-25 20:18:01 +0100

    test: Auth

--- src/use-cases/authenticate.test.ts
+++ src/use-cases/authenticate.test.ts
@@ -3,6 +3,7 @@ import { describe, it, beforeEach, expect } from "vitest";
 import { AuthenticateUseCase } from "./authenticate";
 import { InMemoryOrgsRepository } from "@/Repositories/in-memory/orgs-repository";
 import { hash } from "bcryptjs";
+import { InvalidCredentialsError } from "./Errors/invalid-credencials-error";
 
 let orgsRepository: OrgsRepository;
 let sut: AuthenticateUseCase;
@@ -24,10 +25,46 @@ describe("Authenticate Use Case", () => {
     });
 
     const { org } = await sut.execute({
-      email: "remar@email.com",
+      email: "org@email.com",
       password: "123456",
     });
 
     expect(org.id).toEqual(expect.any(String));
+  });
+
+  it("should not be able to authenticate with wrong email", async () => {
+    await orgsRepository.create({
+      name_of_representative: "Fulano",
+      email: "org@email.com",
+      address: "Vila de Viana ",
+      phone: "+244 930258963",
+      password_hash: await hash("123456", 6),
+      nif: "0489LDN78B",
+    });
+
+    await expect(
+      sut.execute({
+        email: "wrong@email.com",
+        password: "123456",
+      })
+    ).rejects.toBeInstanceOf(InvalidCredentialsError);
+  });
+
+  it("should not be able to authenticate with wrong password", async () => {
+    await orgsRepository.create({
+      name_of_representative: "Fulano",
+      email: "org@email.com",
+      address: "Vila de Viana ",
+      phone: "+244 930258963",
+      password_hash: await hash("123456", 6),
+      nif: "0489LDN78B",
+    });
+
+    await expect(
+      sut.execute({
+        email: "org@email.com",
+        password: "wrong_password",
+      })
+    ).rejects.toBeInstanceOf(InvalidCredentialsError);
   });
 });
